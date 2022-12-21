3fd2f7c52e3a93e5a54362a1fbc935d83bdf7f61
achuriquin <dfghdfgdfg90@gmail.com>
2022-12-21 16:05:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/final_actividad/MainActivity.java
@@ -0,0 +1,59 @@
+
+package com.example.final_actividad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtPass,txtUsu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtUsu = findViewById(R.id.txtUsuario);
+        txtPass = findViewById(R.id.txtPassword);
+
+    }
+    public Connection conexionBD(){
+        Connection cnn=null;
+        try {
+            StrictMode.ThreadPolicy pol=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(pol);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            cnn= DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.109:1433/DESKTOP-L53K4VH;"+
+                    "instance=MSSQLSERVER;user=Hp;password=123");
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+        return cnn;
+
+
+    }
+
+
+        public void Consulta(View view){
+            try {
+                Statement st=conexionBD().createStatement();
+                ResultSet rs=st.executeQuery("SELECT * FROM usuarios where codigo='"+txtUsu.getText().toString()+"'");
+                if(rs.next()){
+
+                    Toast.makeText(getApplicationContext(),"Conexion establecida",Toast.LENGTH_SHORT).show();
+                }
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+
+}
---
+++ app/src/main/java/com/example/final_actividad/frmMainAdmin.java
@@ -0,0 +1,14 @@
+package com.example.final_actividad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frmMainAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_main_admin);
+    }
+}
